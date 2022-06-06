3b67c2e4c7562cb6971e6c78a49355356c6857b9
Jos Ahrens <buughost@gmail.com>
2022-06-06 20:10:53 +0200

    refactor: main
    Co-authored-by: TJ Mazeika <mazeika.tj@gmail.com>

--- main_test.go
+++ main_test.go
@@ -6,16 +6,42 @@ import (
 )
 
 func TestFunc(t *testing.T) {
-	oldStdout := os.Stdout
-	defer func() {
-		os.Stdout = oldStdout
-	}()
+	oldStdin, oldStdout := os.Stdin, os.Stdout
+	stdin, stdout, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+	os.Stdin, os.Stdout = stdin, nil
+	resetStdInOut := func() {
+		os.Stdin, os.Stdout = oldStdin, oldStdout
+	}
 
-	os.Stdout = nil
-	if 0 != demo([]string{"iogo", "--confirm"}) {
+	stdout.WriteString("hello\n")
+	if 0 != demo(flags{}) {
+		resetStdInOut()
 		t.Fail()
 	}
-	if 0 != demo([]string{"iogo", "--help"}) {
+	stdout.WriteString("one\n")
+	if 0 != demo(flags{selectFlag: true}) {
+		resetStdInOut()
 		t.Fail()
 	}
+
+	stdout.WriteString("inp\n")
+	if 0 != demo(flags{progressFlag: true}) {
+		resetStdInOut()
+		t.Fail()
+	}
+
+	if 0 != demo(flags{selectFlag: true, confirmFlag: true, helpFlag: true}) {
+		resetStdInOut()
+		t.Fail()
+	}
+
+	if 1 != demo(flags{selectFlag: true, confirmFlag: true}) {
+		resetStdInOut()
+		t.Fail()
+	}
+
+	resetStdInOut()
 }
